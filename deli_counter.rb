837beab865979294de7c77b26d60645d878c9d9a
Angelina Montanez <angelinamontanez80@gmail.com>
2019-03-05 17:34:52 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,4 +5,8 @@ describe 'Deli Counter' do
   take_a_number(katz_deli, "The line is currently: 1. Logan 2. Avi 3. Spencer")
 
   take_a_number(katz_deli, "The line is currently: 1. Amanda 2. Annette 3. Ruchi 4. Jason 5. Logan 6. Spencer 7. Avi 8. Joe 9. Rachel 10. Lindsey")
-  
+
+  describe "#take_a_number" do
+  take_a_number (katz_deli, "there is nobody in line")
+       
+      
